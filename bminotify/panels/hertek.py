fe8965925ad6f4706041d9291f4f1a24c7638568
Luuk <luukderooij@gmail.com>
2023-10-26 22:40:23 +0200

    Changed read for Penta

--- bminotify/panels/hertek.py
+++ bminotify/panels/hertek.py
@@ -1,9 +1,12 @@
+import time
 import logging
 import asyncio
 
 import emoji
 
+from bminotify import settings
 from bminotify.notify import Bot
+from bminotify.serial import SerialPort
 
 
 logger = logging.getLogger(__name__)
@@ -13,20 +16,75 @@ class Hertek:
         pass
     
     def penta(self, message):
-        if not message.startswith('TOEGANGSNIVEAU') and not message.startswith('Normaal'):
-            print('_______________________________________________\n' + ' \n' + message)
+        message = ''
+        print('hiereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
 
-            if message.startswith('BRAND'):
-                message = emoji.emojize(f':fire: {message}')
-            elif message.startswith('Brandmld Hersteld'):
-                message = emoji.emojize(f':check_mark: {message}')
-            elif message.startswith('Storingen Hersteld'):
-                message = emoji.emojize(f':check_mark: {message}')
-            elif message.startswith('System Normaal'):
-                message = emoji.emojize(f':check_mark: {message}')
-            else:
-                message = emoji.emojize(f':warning: {message}')
+        ser = SerialPort(encoding='ascii', newline=None)
+        ser.open()
+
+        try:
+            while not settings.STOP_READ:
+                try:
+                    data = ser.read()
+                    print(data)
+                    message = data.lstrip()
+                    print(message)
+
+                    # if not message.startswith('TOEGANGSNIVEAU') and not message.startswith('Normaal'):
+                    #         print('_______________________________________________\n' + ' \n' + message)
+                    #         if message.startswith('BRAND'):
+                    #             message = emoji.emojize(f':fire: {message}')
+                    #         elif message.startswith('Brandmld Hersteld'):
+                    #             message = emoji.emojize(f':check_mark: {message}')
+                    #         elif message.startswith('Storingen Hersteld'):
+                    #             message = emoji.emojize(f':check_mark: {message}')
+                    #         elif message.startswith('System Normaal'):
+                    #             message = emoji.emojize(f':check_mark: {message}')
+                    #         else:
+                    #             message = emoji.emojize(f':warning: {message}')
+                    #         logger.info(message)
+                    #         asyncio.get_event_loop().run_until_complete(Bot().send(message))
+                    #         loop = asyncio.new_event_loop()
+                    #         asyncio.set_event_loop(loop)
+                    #         try:
+                    #             loop.run_until_complete(Bot().send(message))
+                    #         except SystemExit:
+                    #             logger.error('Could not send notification!')
+                    #             raise
+                    #         finally:
+                    #             loop.close()
+
+                except Exception as e:
+                    logger.error("Error reading from serial port!")
+                    logger.error(e)
+                    time.sleep(5)
+                    ser.open()
+                    continue
+                if settings.STOP_READ:
+                    logger.info('Closing read thread.')
+
+        except KeyboardInterrupt:
+            pass
+        finally:
+            ser.close()
+
+
+
+
+        # if not message.startswith('TOEGANGSNIVEAU') and not message.startswith('Normaal'):
+        #     print('_______________________________________________\n' + ' \n' + message)
+
+        #     if message.startswith('BRAND'):
+        #         message = emoji.emojize(f':fire: {message}')
+        #     elif message.startswith('Brandmld Hersteld'):
+        #         message = emoji.emojize(f':check_mark: {message}')
+        #     elif message.startswith('Storingen Hersteld'):
+        #         message = emoji.emojize(f':check_mark: {message}')
+        #     elif message.startswith('System Normaal'):
+        #         message = emoji.emojize(f':check_mark: {message}')
+        #     else:
+        #         message = emoji.emojize(f':warning: {message}')
                 
-            logger.info(message)
+        #     logger.info(message)
 
-            asyncio.get_event_loop().run_until_complete(Bot().send(message))
+        #     asyncio.get_event_loop().run_until_complete(Bot().send(message))
